bdf5e16f61657f62fab479991683fb63e4a38e12
KelvinMitaki <mitakikelvin1@gmail.com>
2020-03-09 21:38:01 +0300

    Updates

---
+++ precache-manifest.154a67d840592b3426727aa9239d55d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf8b37e9fdb6eb2b630ed05b2ebc6ba",
+    "url": "/search_videos/index.html"
+  },
+  {
+    "revision": "d6d6e7db3dd5786f4992",
+    "url": "/search_videos/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "6c1e5094554211e009f7",
+    "url": "/search_videos/static/js/2.9cd95f55.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/search_videos/static/js/2.9cd95f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d6e7db3dd5786f4992",
+    "url": "/search_videos/static/js/main.de142bff.chunk.js"
+  },
+  {
+    "revision": "623cacc42ef6180d0c02",
+    "url": "/search_videos/static/js/runtime-main.f81dfecd.js"
+  }
+]);
