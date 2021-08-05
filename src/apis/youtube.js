be9c3113fc8cfc27caf7d200a4226f5534b3ecb0
kelvinmitaki <mitakikelvin1@gmail.com>
2021-08-05 20:51:46 +0300

    added referer

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export const KEY = "AIzaSyBEvMGbGIRLvFm6gJz9Ez_dwBtCSYAVYwk";
 
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3"
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  headers: {
+    "Referrer-Policy": "origin",
+  },
 });
